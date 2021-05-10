83f3db7ea50f5dae0b2dc06712b08f8c04ddd4b0
Ying Huang <huangyingw@gmail.com>
2021-05-09 23:03:53 -0700

    n

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ source "$HOSTRC"
 
 cp -f wifiautoreconnect_template wifiautoreconnect
 sed -i.bak "s|ROUTER_IP_ADDRESS_VAR|$ROUTER_IP_ADDRESS|g" wifiautoreconnect
-mkdir -p ~/Library/Scripts/
-cp -f wifiautoreconnect ~/Library/Scripts/
+mkdir -p /Library/Scripts/
+cp -f wifiautoreconnect /Library/Scripts/
 
-SCRIPT_LOCATION=$(realpath ~/Library/Scripts/wifiautoreconnect)
+SCRIPT_LOCATION=$(realpath /Library/Scripts/wifiautoreconnect)
 cp -f net.martinsoft.wifiautoreconnect.plist_template net.martinsoft.wifiautoreconnect.plist
 sed -i.bak "s|SCRIPT_LOCATION_VAR|$SCRIPT_LOCATION|g" net.martinsoft.wifiautoreconnect.plist
-cp -fv net.martinsoft.wifiautoreconnect.plist ~/Library/LaunchAgents/
+cp -fv net.martinsoft.wifiautoreconnect.plist /Library/LaunchAgents/
 
-launchctl unload ~/Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
-launchctl load ~/Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist
+launchctl unload /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
+launchctl load /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist
