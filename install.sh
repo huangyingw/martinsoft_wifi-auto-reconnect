e156178e383cdefc848b983515af431e342cfcbf
Ying Huang <huangyingw@gmail.com>
2021-05-09 17:02:54 -0700

    n

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
 cp net.martinsoft.wifiautoreconnect.plist ~/Library/LaunchAgents/
 cp wifiautoreconnect ~/Library/Scripts/
 launchctl load ~/Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist
-
