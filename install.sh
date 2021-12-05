321fcc7bae9617b1e81c2eeb214f52cddc5ba842
Ying Huang <huangyingw@gmail.com>
2021-12-04 22:57:07 -0800

    n

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ cd "$SCRIPTPATH"
 
 sudo mkdir -p /Library/Scripts/
 sudo cp -fv wifiautoreconnect.sh /Library/Scripts/
-
-SCRIPT_LOCATION=$(realpath /Library/Scripts/wifiautoreconnect.sh)
-cp -f net.martinsoft.wifiautoreconnect.plist_template net.martinsoft.wifiautoreconnect.plist
-sed -i.bak "s|SCRIPT_LOCATION_VAR|$SCRIPT_LOCATION|g" net.martinsoft.wifiautoreconnect.plist
 sudo cp -fv net.martinsoft.wifiautoreconnect.plist /Library/LaunchAgents/
 
 launchctl unload /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
