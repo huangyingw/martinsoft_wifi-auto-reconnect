7be5c314b79f187dcf050e4e1fbae87a0d3c703b
Ying Huang <huangyingw@gmail.com>
2021-12-04 23:08:58 -0800

    n

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-sudo launchctl unload ~/Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
-sudo launchctl unload /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
+launchctl unload /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist 
 
 sudo rm /Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist
-sudo rm ~/Library/LaunchAgents/net.martinsoft.wifiautoreconnect.plist
-sudo rm /Library/Scripts/wifiautoreconnect
-sudo rm ~/Library/Scripts/wifiautoreconnect
+sudo rm /Library/Scripts/wifiautoreconnect.sh
